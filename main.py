0620629fea9f3a1ebe0eebcb0bd47bddca00691b
thevishnunair <vishnunair689@gmail.com>
2025-05-23 22:56:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from telethon import TelegramClient, events
+import os
+
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+
+source_channel = "tradewixtrust1"
+target_channel = "https://t.me/+Ery86ayi9LpiM2Y1"
+
+client = TelegramClient("stock_session", api_id, api_hash)
+
+@client.on(events.NewMessage(chats=source_channel))
+async def handler(event):
+    message = event.message.message
+    if message and not any(link in message.lower() for link in ["http", "https", "t.me"]):
+        await client.send_message(target_channel, message)
+
+client.start()
+client.run_until_disconnected()
